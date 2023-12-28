3b78e0577e3b8a21b1e12b38494a88275a4aa5ee
Ahmed <nahmed3536@gmail.com>
2023-12-28 12:33:20 +0400

    has name

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ st.set_page_config(page_title="GPTour")
 # set the header
 st.header("GPTour Chat")
 
+### IMPORTS ###
+from typing import Optional
+import random
+
 ### LOGGING ###
 import logging
 log = logging.getLogger(__name__)
@@ -23,40 +27,124 @@ def pretty_print_dict(dictionary):
     return json.dumps(dictionary, indent=4)
 
 
-### AI ASSISTANT CODE ###
+### CONFIG TOML ###
+# access toml configuration
+import toml
+config = toml.load(".streamlit/config.toml")
+
+### OpenAI's ChatGPT API ###
+import os
+import openai
+
+openai_client = openai.OpenAI(
+    api_key=os.environ['OPENAI_API_KEY'],
+)
+
+def chatgpt(prompt: str, context: str = "You are a helpful assistant.", model: str = "gpt-3.5-turbo") -> str:
+    response = openai_client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": context,
+            },
+             {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+        model=model,
+    )
+    return response.choices[0].message.content
+
+### CUSTOM ASSISTANT CODE ###
+def identify_user(prompt: str) -> Optional[str]:
+    """
+    Identifies the user
+    """
+    context = (
+        "Instruction: given the prompt, your goal is to extract the first name of the user. "
+        "Only return the name. If you can't determine it, please return with 'undetermined'. "
+        "Example: if the prompt is 'my name is Mary', answer with 'Mary'. "
+        "Example: if the prompt is 'my friends call me jake', answer with 'Jake'. "
+        "Example: if the prompt is 'i like beaches and sunset', answer with 'undetermined' as there's no name mentioned."
+    )
+
+    result = chatgpt(prompt, context)
+
+    try:
+        user_name = result.strip().lower() # clean the results
+        assert user_name != "undetermined" # make sure it's not undetermined
+
+        user_name = user_name[0].upper() + user_name[1:] # capitalize first letter
+        return user_name
+    except Exception as e:
+        log.info(f"Couldn't parse prompt = {prompt} for name. Found error: {e}")
+
+
 def assistant(prompt: str, context: str = "You are a helpful assistant.") -> str:
     return "Not Programmed Yet!"
 
 context = ""
 
-### AI ASSISTANT CODE ###
+
 
 
 ### WEBSITE CODE ###
+# session variables
+if "user_name" not in st.session_state: st.session_state.user_name = None
+
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I help you?"}]
+    st.session_state.messages = [{
+        "role": "assistant", 
+        "content": (
+            "Hello, I'm GPTour and I'm here to help with your travels. "
+            "Before we begin, what is your name?"
+            )
+    }]
 
 # Display chat messages
 for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
+    with st.chat_message(message["role"], avatar=config["custom"][f"{message['role']}_avatar"]):
         st.write(message["content"])
 
 # User-provided prompt
 if prompt := st.chat_input():
     st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
+    with st.chat_message("user", avatar=config["custom"]["user_avatar"]):
         st.write(prompt)
 
 # Generate a new response if last message is not from assistant
 if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant"):
+    with st.chat_message("assistant", avatar=config["custom"]["assistant_avatar"]):
         with st.spinner("Thinking..."):
-            response = assistant(prompt, context)
+            if not st.session_state.user_name:
+                # first identify for the user's names
+                user_name = identify_user(prompt)
+
+                # update session name
+                if not st.session_state.user_name and user_name:
+                    st.session_state.user_name = user_name
+
+                # update response based on whether the name is found
+                if not st.session_state.user_name:
+                    possible_name_follow_ups = [
+                        "I'm sorry but I couldn't catch your name - could you please share with me?",
+                        "I couldn't catch your name - I'd love to know how to refer to you doing our conversation.",
+                        "Before we begin, it would be helpful to have your name."
+                        "I'm excited to help you with your travels! Before we start, it would be helpful for me to have your name!"
+                    ]
+                    response = random.choice(possible_name_follow_ups)
+                else:
+                    response = f"Welcome {st.session_state.user_name}! I'm here to help you with your travels! ✈️ 🏨 🧳"
+            else:
+                response = assistant(prompt, context)
+            
             st.write(response)
+    
     message = {"role": "assistant", "content": response}
     st.session_state.messages.append(message)
-### WEBSITE CODE ###
+
 
 
 
